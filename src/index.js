6df39f53592da87fa7f620f6948198cec7934357
kimilguk <kimilguk@knou.ac.kr>
2022-08-14 02:55:03 +0000

    basic04교시 작업 소스

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Routes, Route } from "react-router-dom";//npm install react-router-dom
+import ClassKakaoMap from './components/ClassKakaoMap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));기존코드를 주석처리 후 아래코드 추가
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/classkakaomap" element={<ClassKakaoMap />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
